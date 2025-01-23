2a023d97b9e091368ea24df120aba7b1d26ef466
jaspervermeulen <jaspervermeulen@icloud.com>
2025-01-23 11:23:48 +0100

    Initial Commit

---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by Gastgebruiker on 23/01/2025.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
